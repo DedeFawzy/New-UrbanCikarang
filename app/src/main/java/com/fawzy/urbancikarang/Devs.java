55d13a85058a8bf9df2236ef25d0f950bdac2b51
Dede Fawzy <dede.fawzy@gmail.com>
2015-03-11 14:08:17 +0700

    Initial commit

--- app/src/main/java/com/fawzy/urbancikarang/Devs.java
+++ app/src/main/java/com/fawzy/urbancikarang/Devs.java
@@ -1,12 +1,15 @@
 package com.fawzy.urbancikarang;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.ImageButton;
 
 
 public class Devs extends ActionBarActivity {
-    ImageView imageView;
+    ImageButton imageView;
     ImageLoader imgloader;
     String imgurl;
 
@@ -17,28 +20,60 @@ public class Devs extends ActionBarActivity {
         setTitle("The Developers & Contributors");
 
         int loader = R.mipmap.ic_launcher;
-        imageView = (ImageView) findViewById(R.id.imageView1);
+        imageView = (ImageButton) findViewById(R.id.imageView1);
         imgurl = ("https://pbs.twimg.com/profile_images/571856361047085056/d9sdMiVz.png");
         imgloader = new ImageLoader(getApplicationContext());
         imgloader.DisplayImage(imgurl, loader, imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://twitter.com/UrbanCikarang"));
+                startActivity(intent);
+            }
+        });
 
         loader = R.mipmap.ic_launcher;
-        imageView = (ImageView) findViewById(R.id.imageView2);
+        imageView = (ImageButton) findViewById(R.id.imageView2);
         imgurl = ("https://pbs.twimg.com/profile_images/564724879417569280/jlkOhxgn.jpeg");
         imgloader = new ImageLoader(getApplicationContext());
         imgloader.DisplayImage(imgurl, loader, imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://twitter.com/FawzyDede"));
+                startActivity(intent);
+            }
+        });
 
         loader = R.mipmap.ic_launcher;
-        imageView = (ImageView) findViewById(R.id.imageView3);
+        imageView = (ImageButton) findViewById(R.id.imageView3);
         imgurl = ("https://pbs.twimg.com/profile_images/516166443872706561/b3uDjWih.jpeg");
         imgloader = new ImageLoader(getApplicationContext());
         imgloader.DisplayImage(imgurl, loader, imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://twitter.com/VikarELMahdi"));
+                startActivity(intent);
+            }
+        });
 
         loader = R.mipmap.ic_launcher;
-        imageView = (ImageView) findViewById(R.id.imageView4);
+        imageView = (ImageButton) findViewById(R.id.imageView4);
         imgurl = ("https://pbs.twimg.com/profile_images/556025895475900416/-A6Gc_ko.jpeg");
         imgloader = new ImageLoader(getApplicationContext());
         imgloader.DisplayImage(imgurl, loader, imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://twitter.com/Gentong90"));
+                startActivity(intent);
+            }
+        });
 
     }
 }
